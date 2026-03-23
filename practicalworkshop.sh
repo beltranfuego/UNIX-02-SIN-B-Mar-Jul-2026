673767b0fb43e502d468f0fd3f3a01ba81dec7b9
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-03-23 12:39:42 +0000

    que hacen los comandos

--- practicalworkshop.sh
+++ practicalworkshop.sh
@@ -1,9 +1,9 @@
-uname -a
-which gpg
-gpg --version
-gpg --full-generate-key
-gpg --list-keys
-gpg --armor --export beltranartfire@hotmail.com > mi_llave_publica.asc
-cat mi_llave_publica.asc
-gpg --list-secret-keys --keyid-format=long
-gpg --armor --export-secret-keys
+uname -a    #Muestra información del sistema (kernel, arquitectura, etc.)
+which gpg   #Indica la ruta donde está instalado gpg
+gpg --version       #Muestra la versión de GPG y sus características
+gpg --full-generate-key     #Crea un nuevo par de claves (pública y privada)
+gpg --list-keys     #Lista las claves públicas almacenadas
+gpg --armor --export beltranartfire@hotmail.com > mi_llave_publica.asc      #Exporta tu clave pública en formato texto(armor) (.asc)
+cat mi_llave_publica.asc        #Muestra el contenido de la clave pública en pantalla
+gpg --list-secret-keys --keyid-format=long      #Lista tus claves privadas con identificadores largos
+gpg --armor --export-secret-keys        #Exporta tus claves privadas en formato texto (uso delicado)
