0532d52358abe42e4c23a6c463aaef0c49edfac4
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-03-25 13:47:36 +0000

    recorderis 3

--- filesystem.sh
+++ filesystem.sh
@@ -2,3 +2,4 @@ pwd     # print working directory
 cd      # cambia directorios 
 ls -F   # Lista archivos indicando su tipo con símbolos al final
 ls -i   # Muestra el número de inodo de cada archivo
+stat    #sirve para ver toda la información detallada de un archivo o directorio
