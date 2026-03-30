d99eec340e367cea855272d36245bbd4401d3f2d
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-03-30 13:07:32 +0000

    commit 3

--- nuevo.sh
+++ nuevo.sh
@@ -1,4 +1,5 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install parted
+sudo apt update  #Updates the package list from repositories (refreshes available software info).
+sudo apt upgrade    #Upgrades all installed packages to their latest versions.
+sudo apt install parted #Installs the **:contentReference[oaicite:0]{index=0}** tool for managing disk partitions.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
+#Upgrades all installed packages to their latest versions.
