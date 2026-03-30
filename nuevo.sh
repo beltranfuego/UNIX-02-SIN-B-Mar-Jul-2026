0cf0bc10a8515bf999fb6548ced9f1d905ab256b
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-03-30 13:13:53 +0000

    commit 4

--- nuevo.sh
+++ nuevo.sh
@@ -3,3 +3,13 @@ sudo apt upgrade    #Upgrades all installed packages to their latest versions.
 sudo apt install parted #Installs the **:contentReference[oaicite:0]{index=0}** tool for managing disk partitions.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
 #Upgrades all installed packages to their latest versions.
+#parted -l  Lists all disks and partition tables.  
+#&& Executes the next command only if the previous one succeeds.
+#echo -e "\n---\n"  Prints a separator (blank line + --- + blank line).
+#lsblk -f  Shows block devices with filesystem info (UUID, type, mount point).
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
+#This command checks whether the system is booted in **UEFI or BIOS mode**.
+#-d  Tests if the directory `/sys/firmware/efi` exists  
+#&&   Runs the next command **only if the condition is true**
+#echo "UEFI"  Prints **UEFI** if the directory exists
+# ||   Runs the next command **if the previous one fails**
