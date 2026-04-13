f6d81e5d30ea3084615fc799bf8f60005db3e2cb
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-04-13 12:32:37 +0000

    commit1

---
+++ script.sh
@@ -0,0 +1,3 @@
+ls -a #Shows all files in the current directory, including hidden ones (those starting with .)
+ls --all   #Does exactly the same as -a (it’s just the long version of the option)
+ls -a / #Lists all files in the root directory /, including hidden system files
