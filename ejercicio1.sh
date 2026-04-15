447e0b529228c1398763ee66df1bbdb2df2de30c
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-04-15 13:02:48 +0000

    commit firstcommand corrected

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,1 +1,2 @@
-echo '#!/bin/sh' > hola.sh
+echo '#!/bin/sh' > hola.sh  #   Creates or overwrites the file hola.sh and writes the line #!/bin/sh into it. 
+#This is usually used to start a shell script with a shebang, which tells the system to run the script using sh
