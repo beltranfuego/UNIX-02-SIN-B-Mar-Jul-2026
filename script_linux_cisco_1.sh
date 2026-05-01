09db67110e7e93484f9f7b14e0f698aaa39ba6b1
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-01 19:51:14 +0000

    chown command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -19,3 +19,4 @@ exit        #If you are inside another user (like after su), it returns you to t
 sl      #It displays a steam train animation in the terminal, steam locomotive
 sudo        #It allows a permitted user to run commands with elevated privileges (usually as root), super user do.
 chmod       #It is used to change file and directory permissions in Linux.
+chown       #It is used to change the owner and/or group of a file or directory.
