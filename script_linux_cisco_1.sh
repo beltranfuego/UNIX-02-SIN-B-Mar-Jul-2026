b4a82646356f7400bb527296933c1ac9c3124c07
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-01 17:57:02 +0000

    ls -l command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -3,3 +3,4 @@ aptitude        #Runs the Aptitude package manager in interactive mode.
 #It lets you search, install, upgrade, and remove software packages using a text-based interface.
 aptitude moo        #This is a hidden Easter egg inside Aptitude. Instead of doing real package management, 
 #it prints a funny message (usually something like “There are no Easter Eggs in this program” ).
+ls -l       #The command ls -l lists files and directories in long format.
