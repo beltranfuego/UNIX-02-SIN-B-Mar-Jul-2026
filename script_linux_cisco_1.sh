0be717c1a469a55f35c268b754dea8311cab8b84
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-01 17:58:25 +0000

    ls -r command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -4,3 +4,4 @@ aptitude        #Runs the Aptitude package manager in interactive mode.
 aptitude moo        #This is a hidden Easter egg inside Aptitude. Instead of doing real package management, 
 #it prints a funny message (usually something like “There are no Easter Eggs in this program” ).
 ls -l       #The command ls -l lists files and directories in long format.
+ls -r       #It shows the contents of a directory but reverses the sorting order (for example, Z → A instead of A → Z).
