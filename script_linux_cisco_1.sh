b384a8a865c477859acd46c6485ec6519a896f03
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-01 17:56:07 +0000

    aptitude command

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,1 +1,5 @@
 ls ~/Documents      #The command ls ~/Documents lists the contents of the Documents folder in your home directory.
+aptitude        #Runs the Aptitude package manager in interactive mode. 
+#It lets you search, install, upgrade, and remove software packages using a text-based interface.
+aptitude moo        #This is a hidden Easter egg inside Aptitude. Instead of doing real package management, 
+#it prints a funny message (usually something like “There are no Easter Eggs in this program” ).
