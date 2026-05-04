222aed0f3b3ffd829e583690e30838b221b6eb24
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-04 13:36:11 +0000

    commit /etc

--- security.sh
+++ security.sh
@@ -2,3 +2,8 @@ id      # Show current user and groups
 cat /etc/passwd | head -10      # Display first 10 lines of /etc/passwd
 cat /etc/group | head -10       # Display first 10 lines of /etc/group
 groups $USER        # Show groups for current user
+id -u                         # Show current user's UID
+id -g                         # Show current user's primary GID
+id -G                         # Show all group IDs for current user
+cat /etc/group | grep games    # Search for the games group in /etc/group
+cat /etc/gshadow               # Display protected group password information
