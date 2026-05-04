70b6bdeefee6250a2afc18f64f7c7b2bfd51c488
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-04 13:26:23 +0000

    commit $USER

--- security.sh
+++ security.sh
@@ -1,2 +1,4 @@
 id      # Show current user and groups
 cat /etc/passwd | head -10      # Display first 10 lines of /etc/passwd
+cat /etc/group | head -10       # Display first 10 lines of /etc/group
+groups $USER        # Show groups for current user
