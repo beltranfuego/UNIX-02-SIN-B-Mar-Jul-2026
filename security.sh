0da72781f3a3d233d1f84b946d7fe116e379459a
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-06 13:37:03 +0000

    commit chgrp

--- security.sh
+++ security.sh
@@ -66,3 +66,7 @@ touch ~/lab_chgrp/proyectos/app.py
 touch ~/lab_chgrp/proyectos/config.json
 touch ~/lab_chgrp/reportes/informe.txt
 touch ~/lab_chgrp/scripts/deploy.sh
+
+# Change group
+chgrp desarrolladores ~/lab_chgrp/proyectos/app.py
+ls -la ~/lab_chgrp/proyectos                      
