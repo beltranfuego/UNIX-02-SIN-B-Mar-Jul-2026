afe328e095a93092138d32d8f94ddebd9201ac0e
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-27 13:01:13 +0000

    commit 1

---
+++ security2.sh
@@ -0,0 +1,4 @@
+id      #see the principal actual group
+id -gn  #check just the name of the principal group
+touch ~/test_group_heredado.txt     #create a file and see which group it has
+ls -la ~/test_group_heredado.txt    #
