57d43e11940a58659b8db6f13783c3e8d5de6e35
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-05-27 13:06:19 +0000

    commit2

--- security2.sh
+++ security2.sh
@@ -1,4 +1,7 @@
 id      #see the principal actual group
 id -gn  #check just the name of the principal group
 touch ~/test_group_heredado.txt     #create a file and see which group it has
-ls -la ~/test_group_heredado.txt    #
+ls -la ~/test_group_heredado.txt    #see the group it has
+echo "Grupo actual: $(id -gn)"      #prints Grupo actual with the command id -gn
+touch ~/antes_de_newgrp.txt         #same as before
+ls -la ~/antes_de_newgrp.txt        #same as before
