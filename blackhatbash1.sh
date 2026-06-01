6cc037346c225d6c12e7edf7032e2462a9c36e9a
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-06-01 13:03:54 +0000

    commit 3

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,3 +1,6 @@
+shebang    #!/bin/bash
+#!/bin/bash -x      It shows every single command in real time
+bash -r blackhatbash1.sh        #limit dangerous commands within the script
 bash --version
 env
 /bin/bash
