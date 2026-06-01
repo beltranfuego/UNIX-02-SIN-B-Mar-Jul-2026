d99b974bfbb658e7251642287d833464c79c8746
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-06-01 13:29:59 +0000

    commit script2

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,16 +1,18 @@
-#shebang    #!/bin/bash
-#!/bin/bash -x      It shows every single command in real time
+#!/bin/bash         
+# Shebang: tells the system to run this script with Bash
+#!/bin/bash -x      
 set -x
 #bash -r blackhatbash1.sh        #limit dangerous commands within the script
 #bash -n blackhatbash1.sh        #debuggind it finds syntaxis error
+#bash -x blackhatbash1.sh       #it shows every command in real time
 bash --version
 env
-/bin/bash
+#/bin/bash
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
 ps -ef
-htop
+#htop
 df --human-readable
 set +x
---
+++ blackhatbash2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+# All this script does is create a directory, create a file
+# within the directory, and then list the contents of the di rectory.
+set -x
+mkdir mydirectory 
+touch mydirectory/myfile 
+ls -l mydirectory
+set +x
+
+#first result
+#[Arthur_Beltran] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ bash -n blackhatbash2.sh 
+#[Arthur_Beltran] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ 
+
+#second result
+#[Arthur_Beltran] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ bash -x blackhatbash2.sh 
+#+ mkdir mydirectory
+#mkdir: cannot create directory ‘mydirectory’: El fichero ya existe
+#+ touch mydirectory/myfile
+#+ ls -l mydirectory
+#total 0
+#-rw-rw-rw- 1 root root 0 jun  1 13:23 myfile
+#[Arthur_Beltran] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ 
+
+#third result
+#[Arthur_Beltran] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ bash blackhatbash2.sh 
+#+ mkdir mydirectory
+#mkdir: cannot create directory ‘mydirectory’: El fichero ya existe
+#+ touch mydirectory/myfile
+#+ ls -l mydirectory
+#total 0
+#-rw-rw-rw- 1 root root 0 jun  1 13:24 myfile
+#+ set +x
+#[Arthur_Beltran] UNIX-02-SIN-B-Mar-Jul-2026 ✓ $ 
