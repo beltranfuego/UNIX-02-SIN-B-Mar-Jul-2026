cbfd11a891af6205b1fa6ac5949817382b7d7fa9
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-06-01 13:55:06 +0000

    commit final final

--- local_scope_variable.sh
+++ local_scope_variable.sh
@@ -1,9 +1,11 @@
 #!/bin/bash 
-PUBLISHER="No Starch Press" 
+PUBLISHER="No Starch Press"         #global variable works inside the entire code
 print_name(){ 
-    local name 
+    local name          #local variable only works inside the function
     name="Black Hat Bash" 
     echo "${name} by ${PUBLISHER}" 
 } 
-print_name 
+print_name          # Function call: write only the function name to execute it
 echo "Variable ${name} will not be printed because it is a local variable."
+
+
