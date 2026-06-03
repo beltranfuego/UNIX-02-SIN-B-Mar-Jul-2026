b6b8a77e38a29209322f904e2ee54caccf939c53
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-06-03 13:31:31 +0000

    commit y push

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -6,3 +6,10 @@ touch test && touch test123
 (ls; ps; whoami)
 
 lzl || echo "the lzl command failed"
+
+ls -l / &> stdout_and_stderr.txt
+
+ls -l / 1> stdout.txt 2> stderr.txt
+
+lzl 2> error.txt
+cat error.txt
