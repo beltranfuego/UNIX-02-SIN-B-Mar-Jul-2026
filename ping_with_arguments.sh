8d0aa1de54f311550f5d15cfd1263b112a290fcd
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-06-08 12:57:00 +0000

    commit additions to ping.sh

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -4,4 +4,9 @@ SCRIPT_NAME="${0}"
 TARGET="${1}"
 echo "Running the script ${SCRIPT_NAME}..."
 echo "Pinging the target: ${TARGET}..."
-ping "${TARGET}"
+ping -c 3 "${TARGET}"           #-c means the number of pings i want
+echo "The arguments are: $@"
+echo "The total number of arguments is: $#"
+for args in "$@"; do
+ echo "${args}"
+done
