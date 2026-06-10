63e49fd2cd8438615b56d16576088b30cb14b22b
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2026-06-10 12:34:09 +0000

    commit integer comparison 2

--- integer_comparison.sh
+++ integer_comparison.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VARIABLE_ONE="10"
-VARIABLE_TWO="20"
+VARIABLE_ONE="100"
+VARIABLE_TWO="05"
 if [[ "${VARIABLE_ONE}" -gt "${VARIABLE_TWO}" ]]; then
  echo "${VARIABLE_ONE} is greater than ${VARIABLE_TWO}."
 else
